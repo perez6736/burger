aade6e18548cd642ec8c833ac5db925f93346f2c
danny perez <perez6736@gmail.com>
2017-11-23 00:52:13 -0500

    sql connection and orm

---
+++ DB/config/connection.js
@@ -0,0 +1,24 @@
+// dependencies ===================================================== 
+var mysql      = require('mysql');
+
+// DB connection =====================================
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'me',
+    password : 'mysqlppw123',
+    database : 'burgers_db'
+  });
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
+  
