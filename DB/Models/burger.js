bcd3f372f479db6904076e0443e6e0709858ad89
danny perez <perez6736@gmail.com>
2017-11-23 00:57:12 -0500

    psuedo coded the model

---
+++ DB/Models/burger.js
@@ -0,0 +1,14 @@
+// dependencies ===================================================== 
+
+var orm = require("../config/orm.js"); 
+
+// orm functions that return relevant data. 
+
+var burger = {
+
+    all: // get all burger
+    create: // create a burger
+    update: // update existing burger
+    delete: // delete a burger. 
+
+}
