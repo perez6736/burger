f4464198d13630d82210e29464d64cd3864b0e61
danny perez <perez6736@gmail.com>
2017-11-24 15:25:57 -0500

    functionality of site is done

--- Controllers/burgers_controller.js
+++ Controllers/burgers_controller.js
@@ -38,8 +38,16 @@ router.put("/api/burgers/:id", function(req, res) {
     burger.update({
         devoured: req.body.devoured   
     },condition, function(result) {
-        console.log(result);
-    })
-})
+
+        // I needed to send back a response inorder for the callback on client side to run
+
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -23,8 +23,22 @@ $(function () {
     $(".devour").on("click", function(){
         // id of the cat which i made the data attr 
         var id = $(this).data("id");
-        
-        
+        var newDevoured = $(this).data("devoured");
+
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function(){
+                console.log("the burger was upadterd");
+                location.reload();
+            }
+        );
+
     })
         
 
