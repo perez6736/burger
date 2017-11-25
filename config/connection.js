06c544ef58ae7dea99612b979926060cb0c82ff2
danny perez <perez6736@gmail.com>
2017-11-25 02:21:05 -0500

    added real db connection

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,18 @@
 var mysql      = require('mysql');
 
 // DB connection =====================================
+var connection;
 
-// this didnt work 
-// var host = process.env.DBHOST || "localhost";
-// var db = process.env.DB-DATABASE || "burgers_db";
-// var username = process.env.USERNAME || "root";
-// var pw = process.env.PW || "mysqlppw123"
-
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host     : "localhost",
     user     : "root",
     password : "mysqlppw123",
     database : "burgers_db"
   });
+}
 
 // Make connection.
 connection.connect(function(err) {
